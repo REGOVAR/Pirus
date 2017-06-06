276357a4f705a9412332b907ab62cfd64e241cc3
ikit <olivier@labsquare.org>
2017-06-06 10:20:16 +0200

    fix requirement.txt and remove celery old code

--- pirus/pirus_server.py
+++ pirus/pirus_server.py
@@ -16,25 +16,6 @@ if not os.path.exists(TEMPLATE_DIR):
 
 
 
-# Init Celery
-from celery import Celery
-celery_app = Celery('application_worker')
-celery_app.conf.update(
-    BROKER_URL = 'amqp://guest@localhost',
-    # CELERY_RESULT_BACKEND = 'rpc',
-    # CELERY_RESULT_PERSISTENT = False,
-    CELERY_TASK_SERIALIZER = 'json',
-    CELERY_ACCEPT_CONTENT = ['json'],
-    CELERY_RESULT_SERIALIZER = 'json',
-    CELERY_INCLUDE = ['pirus_worker'],
-    CELERY_TIMEZONE = 'Europe/Paris',
-    CELERY_ENABLE_UTC = True,
-)
-
-
-
-
-
 
 
 # Load rest of pirus application shall be done after celery init
